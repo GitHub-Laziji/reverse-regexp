1d5de714bb9d59dfe69398d246549c9eb82d3c36
GitHub-Laziji <c_smtp@163.com>
2020-01-28 02:23:30 +0800

    构造树

--- src/main/java/org/laziji/commons/rereg/model/OrdinaryNode.java
+++ src/main/java/org/laziji/commons/rereg/model/OrdinaryNode.java
@@ -1,5 +1,8 @@
 package org.laziji.commons.rereg.model;
 
+import org.laziji.commons.rereg.RegexpDataGenerator;
+import org.laziji.commons.rereg.exception.RegexpIllegalException;
+
 public class OrdinaryNode extends BaseNode {
 
     private Node proxyNode;
@@ -16,5 +19,69 @@ public class OrdinaryNode extends BaseNode {
     @Override
     public String random() {
         return proxyNode.random();
+    }
+
+    private String findFirst(String expression, int l, int r) throws RegexpIllegalException {
+        if (l == r) {
+            return null;
+        }
+        if (expression.charAt(l) == '\\') {
+            if (l + 1 >= r) {
+                throw new RegexpIllegalException(expression, l + 1);
+            }
+            return expression.substring(l, l + 2);
+        }
+        if (expression.charAt(l) == '[') {
+            int i = l + 1;
+            while (i < r) {
+                if (expression.charAt(i) == ']') {
+                    return expression.substring(l, i + 1);
+                }
+                if (expression.charAt(i) == '\\') {
+                    i++;
+                }
+                i++;
+            }
+            throw new RegexpIllegalException(expression, r);
+        }
+        if (expression.charAt(l) == '{') {
+            int i = l + 1;
+            boolean hasDelimiter = false;
+            while (i < r) {
+                if (expression.charAt(i) == '}') {
+                    if (i - l + 1 < 3 || expression.charAt(i - 1) == ',') {
+                        throw new RegexpIllegalException(expression, i - 1);
+                    }
+                    return expression.substring(l, i + 1);
+                }
+                if (expression.charAt(i) == ',') {
+                    if (hasDelimiter) {
+                        throw new RegexpIllegalException(expression, i);
+                    }
+                    hasDelimiter = true;
+                    i++;
+                    continue;
+                }
+                if (expression.charAt(i) < '0' || expression.charAt(i) >'9'){
+                    throw new RegexpIllegalException(expression, i);
+                }
+                i++;
+            }
+            throw new RegexpIllegalException(expression, r);
+        }
+        if (expression.charAt(l) == '(') {
+            int i = l + 1;
+            while (true) {
+                String result = findFirst(expression, i, r);
+                if (result == null || result.length() == 0 || result.length() + i >= r) {
+                    throw new RegexpIllegalException(expression, i);
+                }
+                i += result.length();
+                if (expression.charAt(i) == ')') {
+                    return expression.substring(l, i + 1);
+                }
+            }
+        }
+        return expression.substring(l, l + 1);
     }
 }
